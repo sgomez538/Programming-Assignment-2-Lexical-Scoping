dd5eeb43b79820570c69309db6d728b1026cdf17
sgomez538 <80227347+sgomez538@users.noreply.github.com>
2021-03-07 19:11:59 -0500

    Add files via upload

---
+++ Matriz.R
@@ -0,0 +1,33 @@
+# # La función makeCacheMatrix almacena en caché la inversa de una matriz y la función
+# # cacheSolve calcula la inversa de la matriz
+
+# # La siguiente función makeCacheMatrix crea un objeto "matriz" especial que puede
+# # caché su inverso.
+
+makeCacheMatrix <- function(x = matrix()){
+  inversa <- NULL
+  set <- function(y){
+    x<<-y
+    inversa<<-NULL
+    
+  }
+  get<-function(){x}
+  setInversa<- function(inversacalculada){inversa<<-inversacalculada}
+  getInversa<- function(){inversa}
+  list(set = set, get = get, setInversa = setInversa, getInversa = getInversa)
+}
+
+# # La siguiente función cacheSolve calcula la inversa de la "matriz" especial
+# # devuelto por makeCacheMatrix arriba.
+
+cacheSolve <- function(x, ...) {
+  inversa <- x$getInversa()
+  if(!is.null(inversa)) {
+    message("getting cached data")
+    return(inversa)
+  }
+  data <- x$get()
+  inversa <- solve(data, ...)
+  x$setInversa(inversa)
+  inversa
+}
